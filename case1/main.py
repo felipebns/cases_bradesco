0dc3b57e16dd1088d4d53acdb10b68e78bb9f5dc
felipebns <felipebns@al.insper.edu.br>
2026-06-01 11:34:28 -0300

    small changes/git ignore

--- case1/main.py
+++ case1/main.py
@@ -3,13 +3,3 @@ from services.pipeline import Pipeline
 if __name__ == "__main__":
     pipeline = Pipeline(transcript_period="q1_2026", previous_transcript="q4_2025")
     pipeline.run()
-
-    # Fluxo: 
-
-    # Ingere os dados
-
-    # Faz 1 call de LLM com dados atuais e dados anteriores
-
-    # Cria json com informações necessárias
-
-    # Gera relatório
